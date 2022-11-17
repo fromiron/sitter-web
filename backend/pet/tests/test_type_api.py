48f8ebc9a80dd377b1ae0e6515015737a5c05fc2
fromiron <artless1112@gmail.com>
2022-11-17 12:36:02 +0900

    間違って削除されたPET Type Api test file復元

---
+++ backend/pet/tests/test_type_api.py
@@ -0,0 +1,144 @@
+"""
+Pet type apiテスト
+"""
+
+from core.models import Pet, Customer, PetType
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+PET_TYPE_URL = reverse('pet:pettype-list')
+
+
+def detail_url(pet_type_id):
+    """顧客detail URL生成 """
+    return reverse('pet:pettype-detail', args=[pet_type_id])
+
+
+def create_user(**params):
+    """ユーザー生成"""
+    return get_user_model().objects.create_user(**params)
+
+
+def create_staff(**params):
+    """staff生成"""
+    return get_user_model().objects.create_staff(**params)
+
+
+def create_customer(**params):
+    """顧客生成"""
+    defaults = {
+        'name': 'testuser1', 'name_kana': 'testuser1_kana',
+        'tel': '001-012-1111', 'tel2': '002-012-1111', 'address': 'address1111'
+    }
+    defaults.update(params)
+    return Customer.objects.create(**defaults)
+
+
+def create_pet(customer, **params):
+    """pet生成"""
+    defaults = {'name': 'testpet1', 'sex': True, 'birth': '2022-11-15'}
+    defaults.update(params)
+
+    return Pet.objects.create(customer=customer, **defaults)
+
+
+class PublicPetTypeAPITests(TestCase):
+    """認証なしユーザーのアクセス制限テスト"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """認証が必要なことを確認するテスト"""
+        res = self.client.get(PET_TYPE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivatePetTypeApiTests(TestCase):
+    """認証済ユーザーテスト"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@example.com',
+            name='testuser',
+            password='password123',
+            is_staff=False
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_is_not_staff(self):
+        """権限不足こと確認するテスト"""
+        res = self.client.get(PET_TYPE_URL)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PrivatePetTypeApiTestsForStaff(TestCase):
+    """スタッフユーザーテスト"""
+
+    def setUp(self):
+        self.user = create_staff(
+            email='staff@example.com',
+            name='staff',
+            password='password123',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.customer = create_customer()
+
+    def test_is_staff(self):
+        """権限不足こと確認するテスト"""
+        res = self.client.get(PET_TYPE_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_pet_type_list(self):
+        """petデータが正しく取得できるかの確認テスト"""
+        pets = [
+            {'name': 'いぬ'},
+            {'name': 'ねこ'},
+            {'name': 'うさぎ'},
+            {'name': 'とり'},
+        ]
+
+        for data in pets:
+            PetType.objects.create(**data)
+
+        res = self.client.get(PET_TYPE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), len(pets))
+        self.assertEqual(res.data[0]['name'], pets[0]['name'])
+        self.assertEqual(res.data[1]['name'], pets[1]['name'])
+        self.assertEqual(res.data[2]['name'], pets[2]['name'])
+
+    def test_create_pet_type(self):
+        """pet type生成テスト"""
+        payload = {'name': 'いぬ'}
+        res = self.client.post(PET_TYPE_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data['name'], payload['name'])
+
+    def test_delete_pet_type(self):
+        """pet type削除テスト"""
+        type = PetType.objects.create(name="いぬ")
+        url = detail_url(type.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        types = PetType.objects.all()
+        self.assertFalse(types.exists())
+
+    def test_update_type(self):
+        """"pet type修正テスト"""
+        type = PetType.objects.create(name="いぬ")
+        payload = {'name': 'ねこ'}
+        url = detail_url(type.id)
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        type.refresh_from_db()
+        self.assertEqual(type.name, payload['name'])
